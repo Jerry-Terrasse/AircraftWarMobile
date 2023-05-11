e9cf7fd7e8e9eeb683cf07406307e706f1d10f05
Terrasse <terrasse@qq.com>
2023-05-11 17:53:45 +0800

    First commit

---
+++ app/src/main/java/edu/hitsz/bullet/EnemyBullet.java
@@ -0,0 +1,13 @@
+package edu.hitsz.bullet;
+
+/**
+ * @Author hitsz
+ */
+public class EnemyBullet extends AbstractBullet {
+
+
+    public EnemyBullet(double locationX, double locationY, double speedX, double speedY, int power) {
+        super(locationX, locationY, speedX, speedY, power);
+    }
+
+}
---
+++ app/src/main/java/edu/hitsz/bullet/HeroBullet.java
@@ -0,0 +1,12 @@
+package edu.hitsz.bullet;
+
+/**
+ * @Author hitsz
+ */
+public class HeroBullet extends AbstractBullet {
+
+    public HeroBullet(double locationX, double locationY, double speedX, double speedY, int power) {
+        super(locationX, locationY, speedX, speedY, power);
+    }
+
+}
